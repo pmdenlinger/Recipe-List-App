f13d343374e66a356689e9c32d8b9c38a3ebf007
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-24 07:50:37 -0700

    Create directory structure, add assets and JSON file.

--- Recipe List App/Recipe_List_App.swift
+++ Recipe List App/Recipe_List_App.swift
@@ -1,5 +1,5 @@
 //
-//  Recipe_List_AppApp.swift
+//  Recipe_List_App.swift
 //  Recipe List App
 //
 //  Created by user on 5/24/21.
@@ -8,7 +8,8 @@
 import SwiftUI
 
 @main
-struct Recipe_List_AppApp: App {
+struct Recipe_List_App
+: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
