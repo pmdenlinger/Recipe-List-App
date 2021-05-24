7109e2a2df23a9be8fdcd0933d097275e3191cc6
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-24 07:23:39 -0700

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by user on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
