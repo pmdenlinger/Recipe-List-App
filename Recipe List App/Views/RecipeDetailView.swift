acc79f987983b41c8a527ad20ab7b0a4168e20bc
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-26 15:45:02 -0700

    Clean up RecipeDetailView

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,76 +10,78 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe: Recipe
-  
+    
     
     @State var selectedServingSize = 2
     var body: some View {
         
-    ScrollView {
-        
-        VStack(alignment: .leading) {
-            // MARK: Recipe Image
-            Image(recipe.image)
-                .resizable()
-                .scaledToFill()
+        ScrollView {
             
-            // MARK: Serving Size Picker
-        VStack (alignment: .leading) {
-            Text("Select your serving size:")
-            Picker("", selection: $selectedServingSize) {
-                Text("2").tag(2)
-                Text("4").tag(4)
-                Text("6").tag(6)
-                Text("8").tag(8)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .frame(width: 160)
-        }
-        .padding()
-            
-            // MARK: Ingredients
             VStack(alignment: .leading) {
-                Text("Ingredients")
-                    .font(.headline)
-                    .padding([.bottom, .top], 5)
                 
-                // MARK: Divider
-                Divider()
+                // MARK: Recipe Image
+                Image(recipe.image)
+                    .resizable()
+                    .scaledToFill()
                 
-                ForEach (recipe.ingredients) {
-                    item in
-                    Text("- " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name)
+                // MARK: Serving Size Picker
+                VStack (alignment: .leading) {
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 160)
                 }
-            }
-            .padding(.horizontal, 10.0)
-            
-            // MARK: Directions
-            VStack(alignment: .leading) {
-                Text("Directions")
-                    .font(.headline)
-                    .padding(.bottom, 5)
+                .padding()
                 
-                ForEach(0..<recipe.directions.count, id: \.self) { index
-                    in
-                    Text(String(index + 1) + ". " + recipe.directions[index])
+                // MARK: Ingredients
+                VStack(alignment: .leading) {
+                    Text("Ingredients")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                    
+                    // MARK: Divider
+                    Divider()
+                    
+                    ForEach (recipe.ingredients) {
+                        item in
+                        Text("- " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
+                    }
+                }
+                .padding(.horizontal, 10.0)
+                .padding(.bottom, 10.0)
+                
+                // MARK: Directions
+                VStack(alignment: .leading) {
+                    Text("Directions")
+                        .font(.headline)
                         .padding(.bottom, 5)
+                    
+                    ForEach(0..<recipe.directions.count, id: \.self) { index
+                        in
+                        Text(String(index + 1) + ". " + recipe.directions[index])
+                            .padding(.bottom, 5)
+                    }
                 }
+                .padding(.horizontal, 10.0)
             }
-            .padding(.horizontal, 10.0)
+            
         }
+        .navigationBarTitle(recipe.name)
         
     }
-    .navigationBarTitle(recipe.name)
     
-}
     
-
-struct RecipeDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        
-        // Create a dummy recipe and pass it into the detail view so that we can see a preview
-        let model = RecipeModel()
-        RecipeDetailView(recipe: model.recipes[0])
+    struct RecipeDetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            
+            // Create a dummy recipe and pass it into the detail view so that we can see a preview
+            let model = RecipeModel()
+            RecipeDetailView(recipe: model.recipes[0])
+        }
     }
-}
 }
