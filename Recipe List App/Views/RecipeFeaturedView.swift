7585b4c9d9e3d3d0678bbcfa1c2a2efb94d5622b
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-26 16:37:57 -0700

    Make Featured View dynamic.

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -9,60 +9,105 @@ import SwiftUI
 
 struct RecipeFeaturedView: View {
     
-    @EnvironmentObject var model: RecipeModel
+    @EnvironmentObject var model:RecipeModel
+    @State var isDetailViewShowing = false
+    @State var tabSelectionIndex = 0
     
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
-                    Text("Featured Recipes")
-                        .bold()
-                        .padding(.leading)
-                        .padding(.top, 40)
-                        .font(.largeTitle)
+        
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+                
+            
+            GeometryReader { geo in
+            
+            TabView (selection: $tabSelectionIndex) {
+                
+                // Loop through each recipe
+                ForEach (0..<model.recipes.count) { index in
                     
-                    GeometryReader { geo in
-                        TabView {
-                            // Loop through each recipe
-                            ForEach(0..<model.recipes.count) { index in
-
-                                // Only show those that should be featured
-                                if model.recipes[index].featured == true {
-
-                                    // Recipe card
-                                    ZStack {
-                                        Rectangle()
-                                            .foregroundColor(.white)
-
-                                        VStack(spacing: 0) {
-                                            Image(model.recipes[index].image)
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .clipped()
-                                            Text(model.recipes[index].image)
-                                                .padding(5)
-
-                                        }
-                                        .frame(width: geo.size.width  - 40, height: geo.size.height - 100, alignment: .center)
-                                        .cornerRadius(15)
-                                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
-                                        .shadow(radius: 10)
-                                    }
+                    // Only show those that should be featured
+                    if model.recipes[index].featured == true {
+                    
+                        // Recipe card button
+                        Button(action: {
+                            
+                            // Show the recipe detail sheet
+                            self.isDetailViewShowing = true
+                            
+                        }, label: {
+                            
+                            // Recipe card
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
                                 }
                             }
-                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-                            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-
+                            
+                        })
+                        .tag(index)
+                        .sheet(isPresented: $isDetailViewShowing) {
+                            // Show the Recipe Detail View
+                            RecipeDetailView(recipe: model.recipes[index])
                         }
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(15)
+                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                        
                     }
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Preparation Time:")
-                        Text("1 hour")
-                        Text("Highlights")
-                            .font(.headline)
-                        Text("Healthy, Hearty")
-                    }
-                    .padding([.leading, .bottom])
                 }
+                
             }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
         }
+        
+            VStack (alignment: .leading, spacing: 10) {
+                
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text(model.recipes[tabSelectionIndex].prepTime)
+                
+                Text("Highlights")
+                    .font(.headline)
+                RecipeHighlights(highlights: model.recipes[tabSelectionIndex].highlights)
+            }
+            .padding([.leading, .bottom])
+        }
+        .onAppear(perform: {
+            setFeaturedIndex()
+        })
+    }
+    
+    func setFeaturedIndex() {
+        
+        // Find the index of first recipe that is featured
+        let index = model.recipes.firstIndex { (recipe) -> Bool in
+            return recipe.featured
+        }
+        tabSelectionIndex = index ?? 0
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+            .environmentObject(RecipeModel())
+    }
+}
